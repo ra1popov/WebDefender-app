9c5d3a1c0c7e5b24dd6c6fa986c7640b6823833f
Roman Popov <ra1.popov@gmail.com>
2025-08-10 02:10:25 +0200

    Initial commit

---
+++ jni/libself/libcurl-android/jni/curl/tests/configurehelp.pm
@@ -0,0 +1,23 @@
+# This is a generated file.  Do not edit.
+
+package configurehelp;
+
+use strict;
+use warnings;
+use Exporter;
+
+use vars qw(
+    @ISA
+    @EXPORT_OK
+    $Cpreprocessor
+    );
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(
+    $Cpreprocessor
+    );
+
+$Cpreprocessor = '/Users/romanpopov/Library/Android/sdk/ndk/26.1.10909125/toolchains/llvm/prebuilt/darwin-x86_64/bin/x86_64-linux-android21-clang -E -isystem /Users/romanpopov/Library/Android/sdk/ndk/26.1.10909125/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include --sysroot=/Users/romanpopov/Library/Android/sdk/ndk/26.1.10909125/toolchains/llvm/prebuilt/darwin-x86_64/sysroot -isystem /Users/romanpopov/Documents/Projects/Android/WebDefender/jni/libself/libcurl-android/jni/build/zlib/x86-64/include -isystem /Users/romanpopov/Documents/Projects/Android/WebDefender/jni/libself/libcurl-android/jni/build/openssl/x86-64/include -isystem /Users/romanpopov/Documents/Projects/Android/WebDefender/jni/libself/libcurl-android/jni/build/openssl/x86-64/include/openssl -DOPENSSL_SUPPRESS_DEPRECATED';
+
+1;
