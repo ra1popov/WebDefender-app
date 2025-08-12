1282aed861c4c60f34dab8a7539ea527f937ad33
Roman Popov <ra1.popov@gmail.com>
2025-08-12 20:04:55 +0200

    Initial commit

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,3 +1,12 @@
 
 #
-include $(all-subdir-makefiles)
+#include $(all-subdir-makefiles)
+
+LOCAL_PATH_ROOT := $(call my-dir)
+
+include $(LOCAL_PATH_ROOT)/bspatch/Android.mk
+include $(LOCAL_PATH_ROOT)/libnative/Android.mk
+
+ifneq ($(FLAVOR),fdroid)
+    include $(LOCAL_PATH_ROOT)/libself/Android.mk
+endif
